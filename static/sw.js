e0fd3f6610796f7174207bd5c81acb9cb194b781
thelarryrutledge <larryjrutledge@gmail.com>
2025-06-24 15:12:43 -0500

    chore: update cache version to 1750795963852

--- static/sw.js
+++ static/sw.js
@@ -1,5 +1,5 @@
 // Cache version updated by deployment script
-const CACHE_VERSION = 1750768636713
+const CACHE_VERSION = 1750795963852
 const CACHE_NAME = `alvu-v${CACHE_VERSION}`
 const STATIC_CACHE_URLS = [
 	'/',
